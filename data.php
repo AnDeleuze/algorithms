2547d43b6bfd7270788f1716dac2b899d052cdf9
andeleuze <katsuya.ando496@gmail.com>
2018-01-12 01:21:25 +0900

    make data class

---
+++ data.php
@@ -0,0 +1,53 @@
+<?php
+
+class Data
+{
+    public function pop(array &$nums)
+    {
+        $n = null;
+        //TODO 連想配列が入ってきたときの処理
+        $nums = array_values($nums);
+
+        if (!empty($nums)) {
+            $max_index = count($nums) - 1;
+            $n         = $nums[$max_index]; 
+            unset($nums[$max_index]);
+        }
+        return $n;
+    }
+
+    public function shift(array &$nums)
+    {
+        $n = null;
+        $nums = array_values($nums);
+
+        if (!empty($nums)) {
+            $n = $nums[0];
+            unset($nums[0]);
+            $nums = array_values($nums);
+        }
+
+        return $n;
+    }
+
+    public function push(array &$nums, $n)
+    {
+        $nums = array_values($nums);
+
+        $nums[] = $n;
+        return $nums;
+    }
+
+    public function unshift(array &$nums, $n)
+    {
+        $results   = [];
+        $results[] = $n;
+
+        foreach ($nums as $num) {
+            $results[] = $num;
+        }
+
+        $nums = $results;
+        return $nums;
+    }
+}
