02e3ab138a5212feae5c74e3c998ef330c594dbf
andeleuze <katsuya.ando496@gmail.com>
2018-01-11 23:07:27 +0900

    change filename and to class

---
+++ compare.php
@@ -0,0 +1,35 @@
+<?php
+class Compare()
+{
+    public function equal($x, $y)
+    {
+        if ($x == $y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function theBigger($x, $y)
+    {
+        if ($y < $x) {
+            return $x;
+        } elseif ($x < $y) {
+            return $y;
+        } else {
+            return false;
+        }
+    }
+
+    public function theSmaller($x, $y)
+    {
+        if ($x < $y) {
+            return $x;
+        } elseif ($y < $x) {
+            return $y;
+        } else {
+            return false;
+        }
+    }
+}
+?>
